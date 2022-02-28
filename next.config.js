195c10430c63d1b2099a169907ab71727916b089
Hussein <hussein.h.tech@gmail.com>
2022-02-28 06:28:25 +0200

    clear env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    SERVICE_ID: "service_2v6cnd9",
-    TEMPLATE_ID: "template_zswke8o",
-    USER_ID: "user_laUisAw4lPS1VrFWbZ2Ad",
-  },
 };
+
 module.exports = nextConfig;
