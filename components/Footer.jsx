6d8275a4296dd8e2a8ccf1ce0bafda372a34d506
Hussein <hussein.h.tech@gmail.com>
2022-03-14 07:22:24 +0200

    update footer with link to svelte site

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,16 @@ export default function Footer() {
     <footer className="mt-12">
       <hr />
       <p className="mt-4">
-        This website is open source. You can check how I built it{" "}
+        I built this website with{" "}
+        <a
+          href="https://nextjs.org"
+          rel="noreferrer"
+          target="_blank"
+          className="font-bold no-underline"
+        >
+          Next.js
+        </a>{" "}
+        & it's open source. You can check how I built it{" "}
         <a
           href="https://github.com/Hussein-Ali-Hassan/Personal-Website/tree/main"
           rel="noreferrer"
@@ -11,6 +20,26 @@ export default function Footer() {
           className="font-bold no-underline"
         >
           here.
+        </a>
+      </p>
+      <p className="mt-4">
+        I have also built this site using{" "}
+        <a
+          href="https://kit.svelte.dev"
+          rel="noreferrer"
+          target="_blank"
+          className="font-bold no-underline"
+        >
+          Svelte
+        </a>{" "}
+        . You can check it at{" "}
+        <a
+          href="https://svelte.husseinhassan.me"
+          rel="noreferrer"
+          target="_blank"
+          className="font-bold no-underline"
+        >
+          svelte.husseinhassan.me
         </a>
       </p>
     </footer>
